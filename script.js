663731e9dfa57496f56171bf934170ffba7fcf1e
gega <gegamesxi2012@gmail.com>
2025-10-15 22:26:44 +0400

    Initial commit: frontend quiz project

---
+++ script.js
@@ -0,0 +1,23 @@
+function checkAnswers() {
+  const answers = {
+    q1:"b", q2:"a", q3:"b", q4:"c", q5:"b",
+    q6:"a", q7:"b", q8:"a", q9:"a", q10:"a",
+    q11:"c", q12:"a", q13:"b", q14:"c", q15:"b",
+    q16:"a", q17:"a", q18:"b", q19:"b", q20:"b"
+  };
+
+  let correctCount = 0;
+
+  for(let q in answers) {
+    const sel = document.querySelector(`input[name="${q}"]:checked`);
+    if(sel && sel.value === answers[q]) correctCount++;
+  }
+
+  // დაფაროს quiz
+  document.getElementById("quiz-box").style.display = "none";
+
+  // აჩვენოს შედეგი
+  const resultDiv = document.getElementById("sedegi");
+  resultDiv.style.display = "block";
+  resultDiv.innerText = `შენი ქულაა: ${correctCount}/20`;
+}
